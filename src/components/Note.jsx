9a735224a0693d510cfbfb3331cb84f4a5bc30ca
Ankit <ph,official.ankit@gmail.com>
2021-05-03 12:26:58 +0530

    UI design

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1 className="note">Title</h1>
+            <p className="note">Content</p>
+        </div>
+    );
+}
+
+export default Note;
