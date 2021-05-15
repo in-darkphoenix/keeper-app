94ee5ac86a416c38f62efe7dadd5c00c76a8f655
Ankit <ph,official.ankit@gmail.com>
2021-05-15 13:51:07 +0530

    v1.0 release

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 function Note(props) {
+    function handleClick() {
+        props.onDelete(props.id);
+    }
+
     return (
         <div className="note">
             <h1 className="note">{props.title}</h1>
             <p className="note">{props.content}</p>
+            <button className="note" onClick={handleClick}>X</button>
         </div>
     );
 }
